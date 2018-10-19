2b04ca5c2ca79c3eeaefb26452dd0d58cbcddfe9
NichoZhang <497086241@163.com>
2018-10-19 14:08:59 +0800

    optimization trap water

--- hard/trappingRainWater.go
+++ hard/trappingRainWater.go
@@ -1,7 +1,33 @@
 package main
 
-// runtime 8 ms, beats 28.26%
+// return 4ms, beats 100%
 func trap(height []int) int {
+	if len(height) == 0 {
+		return 0
+	}
+
+	ans := 0
+	lens := len(height)
+	maxLeft := make([]int, lens)
+	maxRight := make([]int, lens)
+	maxLeft[0] = height[0]
+	for i := 1; i < lens; i++ {
+		maxLeft[i] = max(height[i], maxLeft[i-1])
+	}
+
+	maxRight[lens-1] = height[lens-1]
+	for i := lens - 2; i > 0; i-- {
+		maxRight[i] = max(height[i], maxRight[i+1])
+	}
+	for i := 1; i < lens-1; i++ {
+		ans += min(maxLeft[i], maxRight[i]) - height[i]
+	}
+
+	return ans
+}
+
+// runtime 8 ms, beats 28.26%
+func twoPointTrap(height []int) int {
 	left := 0
 	right := len(height) - 1
 	ans := 0
